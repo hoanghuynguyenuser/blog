9be82bc0191ed436c15ba3a265ff734efbc0c7aa
Hoang Huy Nguyen <hoanghuynguyen.info@gmail.com>
2022-03-19 19:45:14 +0700

    C3 - Complete Static File & SCSS

--- src/index.js
+++ src/index.js
@@ -11,10 +11,11 @@ const port = 3000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
 app.use(morgan('combined'))
 
-// app.engine('handlebars', engine());
-// app.set('view engine', 'handlebars');
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, '/resources/views'));
